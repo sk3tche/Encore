9a46a6960e40a24a5303a213eee0c098cd538b7f
XTZGZoReX <xtzgzorex@gmail.com>
2010-10-27 12:00:26 +0200

    Scripting-related projects.

---
+++ Trinity.Encore.Scripts.AI/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("Trinity Team")]
+[assembly: AssemblyProduct("Encore World of Warcraft Emulator")]
+[assembly: AssemblyCopyright("Copyright © Trinity Team")]
+[assembly: AssemblyTrademark("GNU General Public License v3")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.*")]
