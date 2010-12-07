ea8be5994437ab4dbdbd914064c23e91dbebebbd
XTZGZoReX <xtzgzorex@gmail.com>
2010-12-07 23:03:01 +0100

    Use a shared SolutionInfo.cs for all projects instead of individual AssemblyInfo.cs files.

--- Shared/SolutionInfo.cs
+++ Shared/SolutionInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyCompany("Trinity Team")]
+[assembly: AssemblyCompany("Trinity/Encore Team")]
 [assembly: AssemblyProduct("Encore World of Warcraft Emulator")]
-[assembly: AssemblyCopyright("Copyright © Trinity Team")]
+[assembly: AssemblyCopyright("Copyright © Trinity/Encore Team")]
 [assembly: AssemblyTrademark("GNU General Public License v3")]
 [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0")]
