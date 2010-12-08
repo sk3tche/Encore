3ca9ea3415b9058b7baede27875332004417e1b3
XTZGZoReX <xtzgzorex@gmail.com>
2010-12-08 08:59:09 +0100

    Added project files for the patcher.

---
+++ Trinity.Encore.Tools.Patcher/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Trinity.Encore.Tools.Patcher
+{
+    public static class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
