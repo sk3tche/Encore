4d30ac48a5595e43c233ac9497be098b901d6906
XTZGZoReX <xtzgzorex@gmail.com>
2010-12-13 08:05:25 +0100

    Contracts on Command.

--- Trinity.Encore.Framework.Game/Commands/Command.cs
+++ Trinity.Encore.Framework.Game/Commands/Command.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Diagnostics.Contracts;
 using Trinity.Encore.Framework.Core.Security;
 
 namespace Trinity.Encore.Framework.Game.Commands
 {
+    [ContractClass(typeof(CommandContracts))]
     public abstract class Command
     {
         /// <summary>
@@ -28,7 +30,12 @@ namespace Trinity.Encore.Framework.Game.Commands
 
         public virtual string Description
         {
-            get { return string.Empty; }
+            get
+            {
+                Contract.Ensures(Contract.Result<string>() != null);
+
+                return string.Empty;
+            }
         }
 
         /// <summary>
@@ -38,5 +45,16 @@ namespace Trinity.Encore.Framework.Game.Commands
         /// <param name="sender">The sender of the command (may be null in the case of the console).</param>
         /// <returns>Whether or not arguments were valid.</returns>
         public abstract bool Execute(CommandArguments args, IPermissible sender);
+    }
+
+    [ContractClassFor(typeof(Command))]
+    public abstract class CommandContracts : Command
+    {
+        public override bool Execute(CommandArguments args, IPermissible sender)
+        {
+            Contract.Requires(args != null);
+
+            return false;
+        }
     }
 }
