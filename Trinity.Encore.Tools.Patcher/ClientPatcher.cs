f0d80b81caa923db12ad17ff02a50ee6187be9df
XTZGZoReX <xtzgzorex@gmail.com>
2010-12-23 10:55:35 +0100

    The patcher now patches the Grunt/Battle.net check, to allow users to log in with either an account name or email using Grunt.
    Thanks to Phazerz for the reversing.

--- Trinity.Encore.Tools.Patcher/ClientPatcher.cs
+++ Trinity.Encore.Tools.Patcher/ClientPatcher.cs
@@ -8,7 +8,7 @@ namespace Trinity.Encore.Tools.Patcher
     public sealed class ClientPatcher
     {
         /// <summary>
-        /// The pattern to search for when finding the location to patch.
+        /// The search pattern to find the location to patch the connection index selection.
         /// </summary>
         /// <remarks>
         /// The code to be patched looks like this in client version 4.0.3.13329:
@@ -24,6 +24,27 @@ namespace Trinity.Encore.Tools.Patcher
             {
                 0xE8, null, null, null, null, 0x8B, 0xC8, 0x83, 0xC4, 0x04,
                 0x83, 0xE1, 0x01, 0x80, 0xBC, 0x31, null, null, 0x00, 0x00,
+            };
+
+        /// <summary>
+        /// The search pattern to find the location to patch the Grunt/Battle.net selection.
+        /// </summary>
+        /// <remarks>
+        /// The code to be patched looks like this in client version 4.0.3.13329:
+        /// <code>
+        ///.text:004D5F5D 50C 74 05                     jz      short loc_4D5F64
+        ///.text:004D5F5F 50C BE 01 00 00 00            mov     esi, 1
+        ///.text:004D5F64                               loc_4D5F64:
+        ///.text:004D5F64 50C 8B 0D 84 3F CB 00         mov     ecx, dword_CB3F84
+        ///.text:004D5F6A 50C 85 C9                     test    ecx, ecx
+        ///.text:004D5F6C 50C 74 56                     jz      short loc_4D5FC4
+        ///.text:004D5F6E 50C 8B 01                     mov     eax, [ecx]
+        /// </code>
+        /// </remarks>
+        private static readonly byte?[] _emailCheckPattern =
+            {
+                0x74, null, 0xBE, 0x01, 0x00, 0x00, 0x00, 0x8B, 0x0D, null,
+                null, null, null, 0x85, 0xC9, 0x74, null, 0x8B, 0x01, 0x8B,
             };
 
         private readonly PatternScanner _scanner;
@@ -49,20 +70,27 @@ namespace Trinity.Encore.Tools.Patcher
         /// <summary>
         /// Attempts to patch the client executable.
         /// </summary>
+        /// <param name="patchName">Name of the patch to be applied.</param>
+        /// <param name="pattern">The pattern to search for.</param>
+        /// <param name="replacementBytes">The bytes to insert at the found location.</param>
         /// <returns>A <see>Boolean</see> value indicating whether or not the patching succeeded.</returns>
-        public bool Patch()
+        private bool Patch(string patchName, byte?[] pattern, byte[] replacementBytes)
         {
-            var offset = _scanner.Find(_connectionIndexPattern);
+            Contract.Requires(!string.IsNullOrEmpty(patchName));
+            Contract.Requires(pattern != null);
+            Contract.Requires(replacementBytes != null);
+
+            var offset = _scanner.Find(pattern);
 
             if (offset == null)
             {
-                Console.WriteLine("Offset not found.");
+                Console.WriteLine("{0}: Offset not found.", patchName);
                 return false;
             }
 
             var ofs = (long)offset;
 
-            Console.WriteLine("Offset found at: 0x{0}", ofs.ToString("X8"));
+            Console.WriteLine("{0}: Offset found at: 0x{1}", patchName, ofs.ToString("X8"));
 
             try
             {
@@ -71,14 +99,29 @@ namespace Trinity.Encore.Tools.Patcher
                 {
                     Contract.Assume(ofs >= 0);
                     stream.Position = ofs;
-                    writer.Write(new byte[] { 0xB8, 0x00, 0x00, 0x00, 0x00 });
+                    writer.Write(replacementBytes);
                 }
             }
             catch (Exception ex)
             {
-                Console.WriteLine("Error: {0}", ex.Message);
+                Console.WriteLine("{0}: Error: {1}", patchName, ex.Message);
                 return false;
             }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Attempts to patch the client executable.
+        /// </summary>
+        /// <returns>A <see>Boolean</see> value indicating whether or not the patching succeeded.</returns>
+        public bool Patch()
+        {
+            if (!Patch("Connection index selection", _connectionIndexPattern, new byte[] { 0xB8, 0x00, 0x00, 0x00, 0x00 }))
+                return false;
+
+            if (!Patch("Grunt/Battle.net selection", _emailCheckPattern, new byte[] { 0xEB }))
+                return false;
 
             return true;
         }
