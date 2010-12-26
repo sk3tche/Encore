5ae2b1472efc0640e63424ccb5bf2b7d60bcf54a
silinoron <silinoron@gmail.com>
2010-12-25 19:02:00 -0800

    Another unit test.

--- Trinity.Encore.Tests.Core/UtilityTest.cs
+++ Trinity.Encore.Tests.Core/UtilityTest.cs
@@ -48,7 +48,31 @@ namespace Trinity.Encore.Tests.Core
         [TestMethod]
         public void TestHexToByteArray()
         {
-            // TODO: Write.
+            var str1 = "000102030405060708090A0B0C0D0E0F";
+            var str2 = "000102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F";
+
+            var bytes1 = Utility.HexStringToBinary(bytes1);
+            var bytes2 = Utility.HexStringToBinary(bytes2);
+            var bytes1Reference = new byte[]
+            {
+                0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+                0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
+            };
+
+            vat bytes2Reference = bytes1Reference.Concat(new byte[]
+            {
+                0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
+                0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f,
+            }).ToArray();
+            
+            Assert.AreEqual(bytes1.Length, bytes1Reference.Length);
+            Assert.AreEqual(bytes2.Length, bytes2Reference.Length);
+
+            for (int i = 0; i < bytes1.Length; i++)
+                Assert.AreEqual(bytes1[i], bytes1Reference[i]);
+
+            for (int i = 0; i < bytes2.Length; i++)
+                Assert.AreEqual(bytes2[i], bytes2Reference[i]);
         }
     }
 }
