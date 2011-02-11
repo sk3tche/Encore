590c970ceba179e6c24d47db194452d912636c8b
XTZGZoReX <xtzgzorex@gmail.com>
2011-02-11 15:25:40 +0100

    Extension methods to make it easier to get/set a value at [row, col] in matrices.

--- Trinity.Encore.Game/Mathematics/MathExtensions.cs
+++ Trinity.Encore.Game/Mathematics/MathExtensions.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.Contracts;
 using Mono.GameMath;
 
 namespace Trinity.Encore.Game.Mathematics
@@ -12,6 +13,26 @@ namespace Trinity.Encore.Game.Mathematics
         public static Vector3 ToVector3(this Vector2 vector, float z = 0.0f)
         {
             return new Vector3(vector.X, vector.Y, z);
+        }
+
+        public static unsafe float GetElement(this Matrix matrix, int row, int column)
+        {
+            Contract.Requires(row >= 0);
+            Contract.Requires(row < 4);
+            Contract.Requires(column >= 0);
+            Contract.Requires(column < 4);
+
+            return *(((float*)&matrix) + (4 * row) + column);
+        }
+
+        public static unsafe void SetElement(this Matrix matrix, int row, int column, float value)
+        {
+            Contract.Requires(row >= 0);
+            Contract.Requires(row < 4);
+            Contract.Requires(column >= 0);
+            Contract.Requires(column < 4);
+
+            *(((float*)&matrix) + (4 * row) + column) = value;
         }
     }
 }
