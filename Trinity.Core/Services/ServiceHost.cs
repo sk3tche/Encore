0c7f3c626a003a77814035e630f42aecfd17438c
XTZGZoReX <xtzgzorex@gmail.com>
2011-02-12 16:50:38 +0100

    Unused using directive.

--- Trinity.Core/Services/ServiceHost.cs
+++ Trinity.Core/Services/ServiceHost.cs
@@ -2,7 +2,6 @@ using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Diagnostics.Contracts;
 using System.ServiceModel;
-using System.ServiceModel.Description;
 
 namespace Trinity.Core.Services
 {
