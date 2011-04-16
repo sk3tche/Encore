d2cc799e0902e94f80185c4034ad36c5042ed0cb
Machiavelli <machiavelli.trinity@gmail.com>
2011-04-16 18:35:08 +0200

    Some work on MapService.

---
+++ Trinity.Encore.MapService/Map.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using Trinity.Encore.Game.Partitioning;
+using Trinity.Encore.Game.Entities;
+using Mono.GameMath;
+
+namespace Trinity.Encore.MapService
+{
+    /// <summary>
+    /// This class is instantiated for every "game-instance" of a map.
+    /// All objects on the map are stored in this class.
+    /// DBC-related data is referenced for fast lookup.
+    /// </summary>
+    public class Map
+    {
+        /// <summary>
+        /// Constructs a new instance of this map.
+        /// TODO: Construct based on DBC template
+        /// </summary>
+        public Map(int mapId, long instanceId)
+        {
+            MapId = mapId;
+            InstanceId = instanceId;
+        }
+
+        /// <summary>
+        /// Updates active units and processes events.
+        /// </summary>
+        /// <param name="timeDiff">Time in ms since last call to this function</param>
+        public void Update(int timeDiff)
+        {
+            // Execute scheduled update routines
+        }
+
+        /// <summary>
+        /// Registers an entity on this instance of a map
+        /// </summary>
+        /// <param name="entity"></param>
+        public void AddEntity(IWorldEntity entity)
+        {
+            Contract.Requires(entity != null);
+
+            _entityQuadTree.AddEntity(entity);
+            _entityLookup.Add(entity.Guid, entity);
+        }
+
+        /// <summary>
+        /// Unregisters an entity on this instance of a map
+        /// </summary>
+        /// <param name="entity"></param>
+        public void RemoveEntity(IWorldEntity entity)
+        {
+            Contract.Requires(entity != null);
+
+            _entityQuadTree.RemoveEntity(entity);
+            _entityLookup.Remove(entity.Guid);
+        }
+
+        /// <summary>
+        /// MapId from Map.dbc
+        /// </summary>
+        public int MapId { get; private set; }
+
+        /// <summary>
+        /// Unique identifier for the instance
+        /// </summary>
+        public long InstanceId { get; private set; }
+        
+        /// <summary>
+        /// QuadTree-based storage of Entities
+        /// TODO: Proper bounds
+        /// </summary>
+        private readonly QuadTree _entityQuadTree = new QuadTree(new BoundingBox(new Vector3(-17066, -17066, -17066), new Vector3(17066, 17066, 17066)));
+
+        /// <summary>
+        /// Dictionary-based storage of Entities, for fast guid-based lookup
+        /// </summary>
+        private readonly Dictionary<EntityGuid, IWorldEntity> _entityLookup = new Dictionary<EntityGuid, IWorldEntity>();
+    }
+}
---
+++ Trinity.Encore.MapService/MapManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using Trinity.Encore.Game.Identification;
+
+namespace Trinity.Encore.MapService
+{
+    public class MapManager
+    {
+        /// <summary>
+        /// Update all maps registered to the service.
+        /// </summary>
+        /// <param name="timeDiff">Time in ms since last call to this function.</param>
+        public void Update(int timeDiff)
+        {
+            /// Call Map::Update on all registered map.
+            foreach (KeyValuePair<long, Map> m in _maps)
+                m.Value.Update(timeDiff);
+        }
+
+        /// <summary>
+        /// Returns Map object with specified instanceId
+        /// </summary>
+        /// <param name="instanceId"></param>
+        /// <returns></returns>
+        public Map GetMap(long instanceId)
+        {
+            Contract.Requires(instanceId >= 0);
+            return _maps[instanceId];
+        }
+
+        /// <summary>
+        /// Generate new Map object with given mapId.
+        /// Will generate new instanceId.
+        /// </summary>
+        /// <param name="mapId">Entry in Map.dbc</param>
+        /// <returns></returns>
+        public Map GenerateMap(int mapId)
+        {
+            Contract.Requires(mapId >= 0);
+            return GenerateMap(mapId, (long)_idGen.GenerateId());
+        }
+
+        /// <summary>
+        /// Generate new Map object with given mapId and instanceId.
+        /// </summary>
+        /// <param name="mapId">Entry in Map.dbc</param>
+        /// <param name="instanceId">Assigned unique instance identifier.</param>
+        /// <returns></returns>
+        public Map GenerateMap(int mapId, long instanceId)
+        {
+            Contract.Requires(mapId >= 0);
+            Contract.Requires(instanceId >= 0);
+            Map m = new Map(mapId, instanceId);
+            _maps.Add(instanceId, m);
+            return m;
+        }
+
+        private IdGenerator _idGen;
+        private Dictionary<long, Map> _maps;
+    }
+}
