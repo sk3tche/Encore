677efc00155873efd34f073c5174148efed01de6
XTZGZoReX <xtzgzorex@gmail.com>
2011-05-16 19:55:53 +0200

    Updated Mono.GameMath and built it in SIMD mode (and added Mono.Simd.dll).

--- Trinity.Encore.Game/Mathematics/MathExtensions.cs
+++ Trinity.Encore.Game/Mathematics/MathExtensions.cs
@@ -14,25 +14,5 @@ namespace Trinity.Encore.Game.Mathematics
         {
             return new Vector3(vector.X, vector.Y, z);
         }
-
-        public static unsafe float GetElement(this Matrix matrix, int row, int column)
-        {
-            Contract.Requires(row >= 0);
-            Contract.Requires(row < 4);
-            Contract.Requires(column >= 0);
-            Contract.Requires(column < 4);
-
-            return *(((float*)&matrix) + (4 * row) + column);
-        }
-
-        public static unsafe void SetElement(this Matrix matrix, int row, int column, float value)
-        {
-            Contract.Requires(row >= 0);
-            Contract.Requires(row < 4);
-            Contract.Requires(column >= 0);
-            Contract.Requires(column < 4);
-
-            *(((float*)&matrix) + (4 * row) + column) = value;
-        }
     }
 }
